c677bf07f8a352de60fee78887f88a86a4d80606
Vitor Oliveira <vtr.oliveira@me.com>
2017-01-27 15:45:34 -0200

    Configurações iniciais do projeto

---
+++ src/main/java/br/com/stefanini/WebPrototypeApplication.java
@@ -0,0 +1,12 @@
+package br.com.stefanini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPrototypeApplication.class, args);
+	}
+}
