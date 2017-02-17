82232e7c11f1da4cdb62e5fdd13f7be8294d3677
cielo <cielo@STFALXC039453-W>
2017-02-17 14:10:07 -0200

    [BackEnd] - Ana - Alteração do properties.

--- src/main/java/br/com/stefanini/progress/controller/LoginController.java
+++ src/main/java/br/com/stefanini/progress/controller/LoginController.java
@@ -1,20 +1,14 @@
 package br.com.stefanini.progress.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import br.com.stefanini.progress.model.Login;
-import br.com.stefanini.progress.service.LoginService;
-
 @Controller
 public class LoginController {
 	
-	@Autowired
+	//@Autowired
 //	private LoginService loginService;
 	
 	@RequestMapping(value={"/","/login"}, method = RequestMethod.GET)
