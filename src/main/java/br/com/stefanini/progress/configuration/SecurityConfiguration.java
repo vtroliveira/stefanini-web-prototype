7e09d86871f9eef6bda06208087333ab334bc075
Paulo Junior <Paulo Junior@192.168.25.141>
2017-03-01 11:21:12 -0300

    [FrontEnd] - Paulo Junior - Alterações do security e webConfig

--- src/main/java/br/com/stefanini/progress/configuration/SecurityConfiguration.java
+++ src/main/java/br/com/stefanini/progress/configuration/SecurityConfiguration.java
@@ -2,39 +2,58 @@ package br.com.stefanini.progress.configuration;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@Configuration
+@EnableWebMvcSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Autowired
 	private DataSource dataSource;
 	
-	private String userQuery;
+	@Value("${spring.queries.login-query}")
+	private String loginQuery;
 	
-	private String rolesQuery;
+	@Value("${spring.queries.profile-query}")
+	private String profileQuery;
 	
+	@Override
 	protected void configure(AuthenticationManagerBuilder auth)
 		throws Exception{
 		auth.
 			jdbcAuthentication()
-				.usersByUsernameQuery(userQuery)
-				.authoritiesByUsernameQuery(rolesQuery)
-				.dataSource(dataSource);
-//				.passwordEnconder();
+				.usersByUsernameQuery(loginQuery)
+				.authoritiesByUsernameQuery(profileQuery)
+				.dataSource(dataSource)
+				.passwordEncoder(bCryptPasswordEncoder);
 		
 	}
 	
+	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		
 		http.
 			authorizeRequests()
-				.antMatchers("/").
-					permitAll()
+				.antMatchers("/")
+					.permitAll()
+				.antMatchers("/login")
+					.permitAll()
+				.antMatchers("/registration")
+					.permitAll()
 				.antMatchers("/progress/**")
-					.hasAuthority("")
+					.hasAuthority("USER")
 						.anyRequest()
 				.authenticated()
 			.and()
