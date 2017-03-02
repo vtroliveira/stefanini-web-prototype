57ca71ad783ee9f87c41a30df1c47a7d1abe8904
Paulo Junior <pajunior0@gmail.com>
2017-03-02 17:05:22 -0300

    [FrontEnd] - Paulo Junior - Inclusão na Tela de Login dos Links "Registre-se" e "Esqueceu a Senha", realizada a liberaçã do acesso da pagina "Esqueceu a Senha" na classe Security.

--- src/main/java/br/com/stefanini/progress/configuration/SecurityConfiguration.java
+++ src/main/java/br/com/stefanini/progress/configuration/SecurityConfiguration.java
@@ -43,6 +43,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 				.antMatchers("/").permitAll()
 				.antMatchers("/login").permitAll()
 				.antMatchers("/register").permitAll()
+				.antMatchers("/forgotPassword").permitAll()
 				.antMatchers("/progress/**").hasAuthority("ADM").anyRequest()
 				.authenticated().and().csrf().disable().formLogin()
 				.loginPage("/login").failureUrl("/login?error=true")
