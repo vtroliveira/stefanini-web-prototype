1633915479bdedb0c755e2ad8a510e38e8b406d0
pajunior0 <pajunior0@gmail.com>
2017-03-02 16:25:11 -0300

    [FrontEnd] - Paulo Junior - Correção do Merge

--- src/main/java/br/com/stefanini/progress/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package br.com.stefanini.progress.service;
-
-import br.com.stefanini.progress.model.Login;
-
-public interface UserService {	
-	public Login findLoginByUsername(String username);
-	//public void saveLogin(Login login);
-
-}
