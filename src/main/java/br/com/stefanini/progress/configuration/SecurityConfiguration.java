918b74572c45d48e203b51ca05efcbe7274d7d46
Leandro Rodrigues <leo.rodrig100@gmail.com>
2017-03-02 15:57:44 -0300

    Leandro x9

--- src/main/java/br/com/stefanini/progress/configuration/SecurityConfiguration.java
+++ src/main/java/br/com/stefanini/progress/configuration/SecurityConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{	
